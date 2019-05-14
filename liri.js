eb78d454c15b76e3ca3091d131e52c453b75d4ca
FRED F <FrederickFaour@github.com>
2019-05-13 22:02:00 -0400

    Adding final changes after local testing of liri-node

--- liri.js
+++ liri.js
@@ -1,38 +1,83 @@
 var env = require("dotenv").config();
-//var spotify = new Spotify(keys.spotify);
 var keys = require("./keys.js");
+var spotify = require('node-spotify-api');
+var moment = require("moment");
 var fs = require("fs");
-// Here we incorporate the "axios" npm package
 var axios = require("axios");
+
 // Store the arguments entered into node
 var nodeArgs = process.argv;
 // Store the command used as a variable
 var command = nodeArgs[2];
 var query = nodeArgs.slice(3);
-console.log("Command = " + command);
-console.log("Query = " + query);
+var spotify = new spotify(keys.spotify);
 // Create an empty variable for holding the movie name
 var movieName = "";
+var artist = "";
 
 // List of useable commands
 // concert-this
 // spotify-this-song
 // movie-this
 // do-what-it-says
-//const result = dotenv.config()
+
  
-// if (result.error) {
-//   throw result.error
-// } 
-// console.log(result.parsed)
 
-
-var getMovie = function(query) {
+var getConcert = function(query) {
   // Loop through all the words in the node argument
-  // And do a little for-loop magic to handle the inclusion of "+"s
+  // And do a little for-loop magic to handle the inclusion of "%20" (spaces)
+  for (var i = 0; i < query.length; i++) {
+    if (i > 0 && i < query.length) {
+      artist += "%20" + query[i];
+    }
+    else {
+      artist += query[i];
+    }
+  }
+  // Then run a request with axios to the OMDB API with the movie specified
+  var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+  axios.get(queryUrl).then(
+    function(response) {    
+      if (response.data.length == 0) {
+        console.log("There are no upcoming concerts for " + query.join(" ") + "!");
+      }
+      for (index = 0; index < response.data.length; index++) {
+      console.log("Concert Venue: " + response.data[index].venue.name);
+      console.log("Venue Location: " + response.data[index].venue.city + ", " + response.data[index].venue.region);
+      console.log("Concert Venue: " + moment(response.data[index].datetime).format("MMMM Do YYYY"));
+      console.log("====================================================");
+      } 
+    }
+  )
+};  
+
+var getSong = function(query) {
+  // Use The Sign as the song if nothing is entered
   if (query.length === 0) {
-    query = ["Mr. Nobody"];
+    query = "All that she wants";
   };
+  spotify.search({ type: 'track', query: query }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+    console.log(data.tracks.items[0]);
+  console.log(data.tracks.items.length + " entries were found for this song title:");
+
+  for (i = 0; i < data.tracks.items.length; i++) {
+    console.log("Artists: " + data.tracks.items[i].artists[0].name);
+    console.log("Song name: " + data.tracks.items[i].name);
+    if (data.tracks.items[i].artists[0].preview_url === null) {
+      console.log("No preview URL is given for this entry!");
+    } else {
+      console.log("Preview link: " + data.tracks.items[i].preview_url);
+    }
+    console.log("Album name: " + data.tracks.items[i].album.name);
+    console.log("==============================================================");
+  }
+
+  // Loop through all the words in the query portion of the argument
+  // And do a little for-loop magic to handle the inclusion of "+"s
   for (var i = 0; i < query.length; i++) {
   
     if (i > 0 && i < query.length) {
@@ -42,17 +87,31 @@ var getMovie = function(query) {
       movieName += query[i];
     }
   };
-  console.log(movieName);
+});
+};
+
+var getMovie = function(query) {
+  // USe Mr. Nobody as the film if nothing is entered
+  if (query.length === 0) {
+    query = ["Mr. Nobody"];
+  };
+  // Loop through all the words in the node argument
+  // And do a little for-loop magic to handle the inclusion of "+"s
+  for (var i = 0; i < query.length; i++) {
+  
+    if (i > 0 && i < query.length) {
+      movieName += "+" + query[i];
+    }
+    else {
+      movieName += query[i];
+    }
+  };
   
   // Then run a request with axios to the OMDB API with the movie specified
   var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
   
-  // This line is just to help us debug against the actual URL.
-  console.log(queryUrl);
-  
   axios.get(queryUrl).then(
     function(response) {
-      
       console.log("Movie Title: " + response.data.Title);
       console.log("Release Year: " + response.data.Year);
       console.log("IMDB rating: " + response.data.imdbRating);
@@ -65,17 +124,17 @@ var getMovie = function(query) {
   );
 };
 
-// Switch command to utilize the correct function ansd pass the command
+// Switch command to utilize the correct function and pass the command
 switch
 (command) {
   case
     "concert-this":
-    // do a thing
+    getConcert(query);
   break;
     
   case
     "spotify-this-song":
-    // do a thing
+    getSong(query);
   break;
   
   case
@@ -85,6 +144,22 @@ switch
   
   case
     "do-what-it-says":
-    // do a thing
+    fs.readFile("random.txt", "utf8", function(error, data) {
+
+      // If the code experiences any errors it will log the error to the console.
+      if (error) {
+        return console.log(error);
+      }
+    
+      // We will then print the contents of data
+      console.log(data);
+    
+      // Then split it by commas (to make it more readable)
+      var dataArr = data.split(",");
+    
+      // We will then re-display the content as an array for later use.
+      console.log(dataArr);
+      getSong(dataArr[1]);
+    });
   break;
 }
